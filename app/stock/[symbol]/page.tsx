a4ef3d4233ba2bccabd33e919aae1572925c0744
Zach Marcus <55953922+zachmarcus7@users.noreply.github.com>
2025-05-16 17:30:00 -0700

    mobile test

--- app/stock/[symbol]/page.tsx
+++ app/stock/[symbol]/page.tsx
@@ -10,38 +10,39 @@ export default async function Page(/*props: { params: Promise<{ symbol: string }
   const currentStock = 'AAPL';
 
   return (
-      <div className="flex-grow md:overflow-y-auto shadow-3xl bg-main p-6 md:p-10 xl:p-12">
+    <p>mobile test</p>
+      // <div className="flex-grow md:overflow-y-auto shadow-3xl bg-main p-6 md:p-10 xl:p-12">
 
-        <main className="2xl:px-4 3xl:px-20 4xl:px-54">
+      //   <main className="2xl:px-4 3xl:px-20 4xl:px-54">
 
-          <h1 className="mb-4 font-extrabold text-2xl md:text-3xl">Overview</h1>
-          <h6 className="text-greyish-300 mb-4 text-sm 2xl:text-base">
-            Welcome to Long Term Track!{' '}
-            <span>
-              <span className={`pl-1 font-extrabold text-emerald-500`}>{currentStock}</span> is the current selected stock.
-            </span>
-          </h6>
+      //     <h1 className="mb-4 font-extrabold text-2xl md:text-3xl">Overview</h1>
+      //     <h6 className="text-greyish-300 mb-4 text-sm 2xl:text-base">
+      //       Welcome to Long Term Track!{' '}
+      //       <span>
+      //         <span className={`pl-1 font-extrabold text-emerald-500`}>{currentStock}</span> is the current selected stock.
+      //       </span>
+      //     </h6>
 
-          {/* Top Row */}
-          <Suspense fallback={<DashboardTopPanelsSkeleton />}>
-            <DashboardTopPanels stockSymbol={currentStock} />
-          </Suspense>
+      //     {/* Top Row */}
+      //     <Suspense fallback={<DashboardTopPanelsSkeleton />}>
+      //       <DashboardTopPanels stockSymbol={currentStock} />
+      //     </Suspense>
 
-          {/* Bottom Row */}
-          <div className="mt-6 grid gap-6 grid-cols-1 md:grid-cols-4">
+      //     {/* Bottom Row */}
+      //     <div className="mt-6 grid gap-6 grid-cols-1 md:grid-cols-4">
 
-            <Suspense fallback={<DashboardOverviewPanelsSkeleton />}>
-              <DashboardOverviewPanels stockSymbol={currentStock} />
-            </Suspense>
+      //       <Suspense fallback={<DashboardOverviewPanelsSkeleton />}>
+      //         <DashboardOverviewPanels stockSymbol={currentStock} />
+      //       </Suspense>
 
-            <Suspense fallback={<RecommendationsPanelSkeleton />}>
-              <RecommendationsPanel stockSymbol={currentStock} />
-            </Suspense>
+      //       <Suspense fallback={<RecommendationsPanelSkeleton />}>
+      //         <RecommendationsPanel stockSymbol={currentStock} />
+      //       </Suspense>
 
-          </div>
+      //     </div>
 
-        </main>
+      //   </main>
 
-      </div>
+      // </div>
   );
 }
